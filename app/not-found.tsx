eca19589c04466c4efcfecf06301b1b7ee2bda20
InternetBowser <102930366+InternetBowser670@users.noreply.github.com>
2025-05-12 14:24:55 -0500

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,3 @@
 export default function NotFoundPage() {
-	return (<div className="flex justify-center items-center"><h1 className="text-3xl">Page not found</h1></div>div>);
+	return (<div className="flex justify-center items-center"><h1 className="text-3xl">Page not found</h1></div>);
 }

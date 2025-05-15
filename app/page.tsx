c7bb99a823ffbf21971b3b53e41d593ecda392b4
Joshua <joshua.ghattas@gmail.com>
2025-05-15 16:19:23 -0500

    Fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -98,6 +98,8 @@ export default function Page() {
           onClick={redirectToGames}
         >
           <Image
+            height={200}
+            width={200}
             src={currentImage.src}
             alt={currentImage.caption}
             className="h-[200px] border-[5px] border-solid border-[#ffffff] rounded-[12px] shadow-[0_12px_35px_rgba(255,255,255,0.3)] transition-all duration-300 ease-in-out hover:shadow-[0_20px_50px_rgba(255,255,255,0.4)] hover:scale-105 transform"

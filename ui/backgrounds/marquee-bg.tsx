822590589897d732ed358c570e90bc0995a997b4
Joshua <joshua.ghattas@gmail.com>
2025-05-27 17:18:30 -0500

    Made a small tweak to the marquee: - rotated 12 degrees

--- ui/backgrounds/marquee-bg.tsx
+++ ui/backgrounds/marquee-bg.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const MarqueeRow = () => {
   return (
     <Marquee
-      speed={200}
+      speed={250}
       autoFill={true}
       pauseOnHover={true}
       className="flex-1 h-1/5 z-[0] flex items-center text-[10vh] font-bold text-white overflow-y-hidden"
@@ -25,14 +25,16 @@ const MarqueeRow = () => {
 
 export default function MarqueeBg() {
   return (
-    <div className="absolute inset-0 z-[0] opacity-75 overflow-hidden">
-      <div className="w-full h-full">
-        <div className="flex flex-col h-full">
-          <MarqueeRow />
-          <MarqueeRow />
-          <MarqueeRow />
-          <MarqueeRow />
-          <MarqueeRow />
+    <div className="absolute inset-0 z-0 overflow-hidden">
+      <div className="absolute -inset-[20%] -rotate-12 opacity-75">
+        <div className="w-full h-full">
+          <div className="flex flex-col h-full">
+            <MarqueeRow />
+            <MarqueeRow />
+            <MarqueeRow />
+            <MarqueeRow />
+            <MarqueeRow />
+          </div>
         </div>
       </div>
     </div>

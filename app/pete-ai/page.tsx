fa0c3b04642789865aa5af89b034240ed2a5aa0d
Joshua <joshua.ghattas@gmail.com>
2025-06-17 19:16:49 -0500

    Fixed some more CSS (doing small commits to make quick progress)

--- app/pete-ai/page.tsx
+++ app/pete-ai/page.tsx
@@ -15,15 +15,15 @@ export default function Chat() {
       <div className="flex flex-col items-center h-full relative w-full bg-[#0A1D37] text-white overflow-hidden">
         <MarqueeBg className="opacity-50" />
         <div className="flex flex-col items-center justify-between w-full h-full z-1">
-          <div className="h-[90%] px-2! w-1/2 overflow-y-scroll [scrollbar-color:#808080_white] bg-blue-950 rounded-b-2xl pb-3!">
+          <div className="h-[90%] px-2! w-3/4 overflow-y-scroll [scrollbar-color:#808080_white] bg-blue-950 rounded-b-2xl pb-3! pt-3!">
             {messages.map((message) => (
-              <div key={message.id} className="flex whitespace-pre-wrap">
+              <div key={message.id} className="flex my-2! whitespace-pre-wrap">
                 {message.role === "user" ? "User: " : "AI: "}
                 {message.parts.map((part, i) => {
                   switch (part.type) {
                     case "text":
                       return (
-                        <span className="mx-2!" key={`${message.id}-${i}`}><MemoizedMarkdown id={message.id + "-" + i} content={part.text} /></span>
+                        <span className="mx-2!" key={`${message.id}-${i}`}><MemoizedMarkdown className="" id={message.id + "-" + i} content={part.text} /></span>
                       );
                   }
                 })}
--- ui/memoized-markdown.tsx
+++ ui/memoized-markdown.tsx
@@ -1,32 +1,48 @@
-import { marked } from 'marked';
-import { memo, useMemo } from 'react';
-import ReactMarkdown from 'react-markdown';
+import { marked } from "marked";
+import { memo, useMemo } from "react";
+import ReactMarkdown from "react-markdown";
 
 function parseMarkdownIntoBlocks(markdown: string): string[] {
   const tokens = marked.lexer(markdown);
-  return tokens.map(token => token.raw);
+  return tokens.map((token) => token.raw);
 }
 
 const MemoizedMarkdownBlock = memo(
-  ({ content }: { content: string }) => {
-    return <ReactMarkdown>{content}</ReactMarkdown>;
+  ({ content, className }: { content: string; className?: string }) => {
+    return (
+      <div className={`${className} mb-4!`}>
+        <ReactMarkdown>{content}</ReactMarkdown>
+      </div>
+    );
   },
   (prevProps, nextProps) => {
     if (prevProps.content !== nextProps.content) return false;
     return true;
-  },
+  }
 );
 
-MemoizedMarkdownBlock.displayName = 'MemoizedMarkdownBlock';
+MemoizedMarkdownBlock.displayName = "MemoizedMarkdownBlock";
 
 export const MemoizedMarkdown = memo(
-  ({ content, id }: { content: string; id: string }) => {
+  ({
+    content,
+    id,
+    className,
+  }: {
+    content: string;
+    id: string;
+    className?: string;
+  }) => {
     const blocks = useMemo(() => parseMarkdownIntoBlocks(content), [content]);
 
     return blocks.map((block, index) => (
-      <MemoizedMarkdownBlock content={block} key={`${id}-block_${index}`} />
+      <MemoizedMarkdownBlock
+        className={`${className ?? ""}`}
+        content={block}
+        key={`${id}-block_${index}`}
+      />
     ));
-  },
+  }
 );
 
-MemoizedMarkdown.displayName = 'MemoizedMarkdown';
+MemoizedMarkdown.displayName = "MemoizedMarkdown";

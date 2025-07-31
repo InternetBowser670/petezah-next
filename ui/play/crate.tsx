223cc1305b6ffaecfb6885cca0ae95fd886f376b
Joshua <joshua.ghattas@gmail.com>
2025-07-31 18:05:48 -0500

    Made the widgetbot crate disappear on unload

--- ui/play/crate.tsx
+++ ui/play/crate.tsx
@@ -1,23 +1,47 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function WidgetBotCrate() {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
+    const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/@widgetbot/crate@3";
     script.async = true;
     script.defer = true;
+
     script.onload = () => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      new window.Crate({
-        server: '1337108365591187640',
-        channel: '1345454451904745673',
-        css: 'margin-bottom: 8px;',
+      const crate = new window.Crate({
+        server: "1337108365591187640",
+        channel: "1345454451904745673",
+        css: "margin-bottom: 8px;",
       });
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      window.__crateInstance = crate;
     };
+
     document.body.appendChild(script);
+
+    return () => {
+      const iframe = document.querySelector('iframe[src*="widgetbot"]');
+      if (iframe?.parentElement) {
+        iframe.parentElement.removeChild(iframe);
+      }
+
+      const scriptTag = document.querySelector(
+        'script[src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3"]'
+      );
+      if (scriptTag?.parentElement) {
+        scriptTag.parentElement.removeChild(scriptTag);
+      }
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      delete window.__crateInstance;
+    };
   }, []);
 
   return null;
